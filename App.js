362a0083322b12510ac3b9b0defac7b6d146d3fd
Tanmay Shelar <1110tanmay@gmail.com>
2025-02-23 14:40:59 -0700

    Final.

--- App.js
+++ App.js
@@ -10,44 +10,42 @@ import {
 export default class App extends Component {
   state = {
     results: '',
-    selectedGrade: '',
+    Grade : '',
   };
 
-  onload = async (grades) => {
-    this.setState({ results: 'Loading, please wait...', selectedGrade: grades });
+  onLoad = async (grades) => {
+    this.setState({ results: `Loading, please wait...`, Grade: grades });
 
-    onLoad = async (grades) => {
-      this.setState({ results: 'Loading, please wait...', selectedGrade: grades });
-      
-      const response = await fetch(`https://2s4b8wlhik.execute-api.us-east-1.amazonaws.com/studentData?grade=${grades}`, {
-        method: 'GET',
-      });
-  
-      const results = await response.text();
-      this.setState({ results });
+    const response = await fetch(
+      `https://2s4b8wlhik.execute-api.us-east-1.amazonaws.com/studentData?grade=${grades}`,
+      { method: 'GET' }
+    );
+
+    const data = await response.json(); 
+    this.setState({ results: data.join('\n') });
   };
 
   render() {
-    const { results, selectedGrade } = this.state;
-
+    const { results, Grade } = this.state;
+    
     return (
       <View style={styles.container}>
+        <Text style={styles.header}>
+          {Grade ? `Students who received a/an ${Grade} grade:` : 'Select a grade to view students'}
+        </Text>
+
+        <TextInput
+          style={styles.preview}
+          value={results}
+          placeholder="Results..."
+          editable={false}
+          multiline
+        />
+
         <View>
-          <Text style={styles.heading}>
-            {selectedGrade ? `Students who received a/an ${selectedGrade} grade:` : 'Select a grade to load students'}
-          </Text>
-
-          <TextInput
-            style={styles.preview}
-            value={results}
-            placeholder="Results..."
-            editable={false}
-            multiline
-          />
-
-          {['A', 'B', 'C', 'D', 'E'].map((grades) => (
-            <TouchableOpacity key={grades} onPress={() => this.onload(grades)} style={styles.btn}>
-              <Text>Grade {grades}</Text>
+          {['A', 'B', 'C', 'D', 'E'].map((grade) => (
+            <TouchableOpacity key={grade} onPress={() => this.onLoad(grade)} style={styles.btn}>
+              <Text>Grade {grade}</Text>
             </TouchableOpacity>
           ))}
         </View>
@@ -63,7 +61,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#fff',
   },
-  heading: {
+  header: {
     fontSize: 16,
     fontWeight: 'bold',
     marginBottom: 10,
@@ -76,14 +74,13 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 5,
     color: '#333',
-    marginBottom: 10,
-    textAlignVertical: 'top',
+    marginBottom: 20,
   },
   btn: {
     backgroundColor: '#3498db',
-    padding: 10,
-    borderRadius: 3,
-    marginTop: 10,
+    padding: 20,
+    borderRadius: 5,
+    marginTop: 5,
     alignItems: 'center',
   },
 });
